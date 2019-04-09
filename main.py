f28bb602ed4038dbe25067c4a0f68d1f388f1103
yayassktas <ilyaskater@gmail.com>
2019-04-09 19:50:42 +0200

    mardi

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import requests
 
-r = requests.get("https://fr.openfoodfacts.org/api/v0/produit/3029330003533.json")
-print(r.text)
+product_list = ['Snacks','Alcool']
+def loadp():
+	for i in range(0,2):
+		payload = {'action': 'process',
+		'tagtype_0': 'categories',
+		'tag_contains_0': 'contains',
+		'tag_0': product_list[i],
+		'countries': 'France',
+		'page_size': '1000',
+		'json': 1
+		} 
+		reponse = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', \
+		params=payload)
+		data = reponse.json() 
+		print(reponse.json())
+
+
+loadp()
+
