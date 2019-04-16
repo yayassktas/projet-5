1a3dbea6d42b89ca44e180b5a6cefef6982569c2
yayassktas <43137398+yayassktas@users.noreply.github.com>
2019-04-16 11:44:55 +0200

    Delete sql.py

--- sql.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='yayass', password='bermuda',
-                              host='127.0.0.1',
-                              database='elevage')
-cnx.close()
